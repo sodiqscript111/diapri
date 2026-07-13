c05156eb872b7e7c80f1823f4d29dbefa4e5e74d
sodiqscript111 <simidusodiqt@gmail.com>
2026-07-13 19:02:57 +0100

    Fix CSS format key error in header

--- app/header.py
+++ app/header.py
@@ -7,10 +7,8 @@ def app():
     st.markdown(st_style, 
             unsafe_allow_html=True)
 
-    st.markdown(footer.format(get_text('footer_text'), get_text('github')), 
-                unsafe_allow_html=True)
+    footer_text = footer.replace('{0}', get_text('footer_text')).replace('{1}', get_text('github'))
+    st.markdown(footer_text, unsafe_allow_html=True)
 
-
-    st.markdown(head.format(get_text('app_title'), get_text('app_subtitle')), 
-        unsafe_allow_html=True
-    )
+    head_text = head.replace('{0}', get_text('app_title')).replace('{1}', get_text('app_subtitle'))
+    st.markdown(head_text, unsafe_allow_html=True)
